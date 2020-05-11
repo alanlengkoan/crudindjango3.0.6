5495b152458b8a79fd4bdab155f7aa65e9c9844b
Alan Lengkoan <alanlengkoan15@gmail.com>
2020-05-11 21:04:16 +0800

    crud

--- show/urls.py
+++ show/urls.py
@@ -5,6 +5,21 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('tampil/', views.tampil, name='tampil'),
+
+    # untuk menampilkan  data
+    path('lihat/', views.lihat, name='lihat'),
+
+    # untuk menampilkan form tambah
+    path('lihat/add', views.add, name='add'),
+    path('lihat/add/add_post', views.add_post, name='add_post'),
+
+    # untuk menapilkan form ubah
+    path('lihat/upd/<int:id>', views.upd, name='upd'),
+    path('lihat/upd/upd_post', views.upd_post, name='upd_post'),
+
+    # untuk menghapus data
+    path('lihat/del/<int:id>', views.trush, name='trush'),
+
+    # untuk menampilkan detail
     path('detail/<int:id>', views.detail, name='detail'),
 ]
--- show/views.py
+++ show/views.py
@@ -2,13 +2,13 @@
 from django.http import HttpResponse
 # untuk mengrender .html
 from django.shortcuts import render
+# untuk redirect
+from django.shortcuts import redirect
 # untuk mengambil semua models
 from . import models
 
 # untuk halaman index
 def index(request):
-    # return HttpResponse('halaman tentang')
-
     # untuk mengambil data dari database
     result = models.Tampil.objects.all()
 
@@ -19,15 +19,14 @@ def index(request):
             ['/tentang', 'Tentang'],
             ['/kontak', 'Kontak'],
             ['/show', 'Show'],
-            ['/show/tampil', 'Tampil']
+            ['/show/lihat', 'Lihat']
         ],
         'result': result
     }
     return render(request, 'show/index.html', data)
 
-def detail(request, id):
-    # return HttpResponse('halaman tentang')
-    
+# untuk halaman detail
+def detail(request, id):    
     # untuk mengambil data dari database
     result = models.Tampil.objects.get(pk=id)
     
@@ -43,8 +42,88 @@ def detail(request, id):
 
     return render(request, 'show/detail.html', data)
 
-def tampil(request):
-    # return HttpResponse('halaman tentang')
-    return HttpResponse('halaman tampil')
+# untuk halaman lihat data
+def lihat(request):
+    # untuk mengambil semua data
+    result = models.Tampil.objects.all()
 
+    data = {
+        'title': 'Lihat data',
+        'halaman': 'Lihat data',
+        'menu': [
+            ['/tentang', 'Tentang'],
+            ['/kontak', 'Kontak'],
+            ['/show', 'Show'],
+            ['/show/lihat', 'Lihat']
+        ],
+        'result': result
+    }
+    return render(request, 'show/lihat.html', data)
 
+# untuk tambah data
+def add(request):
+    data = {
+        'title': 'Tambah data',
+        'halaman': 'Tambah data',
+        'menu': [
+            ['/tentang', 'Tentang'],
+            ['/kontak', 'Kontak'],
+            ['/show', 'Show'],
+            ['/show/lihat', 'Lihat']
+        ],
+    }
+
+    return render(request, 'show/add.html', data)
+
+# untuk proses tambah
+def add_post(request):
+
+    if request.method == 'POST':
+        tampil = models.Tampil()
+        tampil.title = request.POST['inp_judul']
+        tampil.body = request.POST['inp_isi']
+        tampil.save()
+    
+    return redirect('/show/lihat/')
+
+# untuk ubah data
+def upd(request, id):
+    # untuk mengambil data dari database
+    result = models.Tampil.objects.get(pk=id)
+
+    data = {
+        'title': 'Tambah data',
+        'halaman': 'Tambah data',
+        'menu': [
+            ['/tentang', 'Tentang'],
+            ['/kontak', 'Kontak'],
+            ['/show', 'Show'],
+            ['/show/lihat', 'Lihat']
+        ],
+        'result': result
+    }
+
+    return render(request, 'show/upd.html', data)
+
+# untuk proses update
+def upd_post(request):
+
+    if request.method == 'POST':
+        id = request.POST['inp_id']
+        
+        tampil = models.Tampil.objects.get(id=id)
+        tampil.title = request.POST['inp_judul']
+        tampil.body = request.POST['inp_isi']
+        tampil.save()
+    
+    return redirect('/show/lihat/')
+
+# untuk proses hapus
+def trush(request, id):
+    models.Tampil.objects.get(id=id).delete()
+    
+    return redirect('/show/lihat/')
+
+# def tampil(request):
+#     # return HttpResponse('halaman tentang')
+#     return HttpResponse('halaman tampil')
